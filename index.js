4aecfbc1ef3e329830c6af1bc364d618bf3972a6
sunita608 <143607954+sunita608@users.noreply.github.com>
2023-11-13 13:46:05 +0600

    newsbd

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// typed js
+
+var typed = new Typed('#typed', {
+    strings: ['NEWsBD WEBsITE '],
+    typeSpeed: 90,
+    loop:true,
+    loopCount: Infinity,
+  
+  });
+  
+
+  var slides = document.getElementsByClassName("slideitem");
+  for(i = 0; i < slides.lenght; i++) {
+    slides[i].style.display = "none";
+  }
+
+
+
+
+
+  // 2nd card
+
+
+  // end
